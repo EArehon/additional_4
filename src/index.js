0b072b8d8825152f33c421b6b730a1addc3618c1
EArehon <earehon@gmail.com>
2017-09-28 14:16:04 +0300

    second try

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function multiply(first, second) {
-  return '50';
-}
+		let a = first.split('').reverse();
+		let b = second.split('').reverse();
+		
+		let comp = [];
+		
+		for (let i = 0; i < a.length+b.length; i++)
+		{
+			comp[i] = 0;
+		}
+		
+		for (let i = 0; i < a.length; i++){
+			for (let j = 0; j < b.length; j++){
+				comp[i+j] += (a[i] * b[j]) % 10;
+				comp[i+j+1] += Math.floor((a[i] * b[j]) / 10);
+				
+				if(comp[i+j] >= 10){
+					let temp = comp[i+j];
+					comp[i+j] %= 10;
+					comp[i+j+1] += Math.floor(temp / 10);
+				}
+				
+				if(comp[i+j+1] >= 10){
+					let temp = comp[i+j+1];
+					comp[i+j+1] %= 10;
+					comp[i+j+2] += Math.floor(temp / 10);
+				}
+			}
+		}
+
+		return comp.reverse();
+	}
